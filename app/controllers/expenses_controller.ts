7a33d6165de9cce8f2253a8402f47893392c08b5
JocasChitende31 <84420296+JocasChitende31@users.noreply.github.com>
2024-09-11 13:36:53 +0100

    Exponses controller created with all http methods = index-get, store-post, show-getById, update-put and destroy-delete

--- app/controllers/expenses_controller.ts
+++ app/controllers/expenses_controller.ts
@@ -1,4 +1,57 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default class ExpensesController {
+  async index() {
+    const expenses = await prisma.expenses.findMany()
+    try {
+      if (expenses.length <= 0) return 'There is no expense available'
+      return expenses
+    } catch (err) {
+      throw err.message
+    }
+  }
+  async store({ request }: HttpContext) {
+    const data = request.only(['description', 'invoceRef', 'cost', 'dateOfBuying', 'expensesKind'])
+    try {
+      if (data !== null) {
+        const expense = await prisma.expenses.create({ data })
+        return `Expenses created \n ${expense} \n successfully`
+      }
+      return data
+    } catch (err) {
+      throw err.message
+    }
+  }
+  async show({ params }: HttpContext) {
+    const expenseCondiction = { where: { id: `${params.id}` } }
+    const expense = await prisma.expenses.findUnique(expenseCondiction)
+    try {
+      return expense
+    } catch (err) {
+      throw err.message
+    }
+  }
+  async update({ params, request }: HttpContext) {
+    const data = request.only(['description', 'invoceRef', 'cost', 'dateOfBuying', 'expensesKind'])
+    const updateExpenseCondiction = { where: { id: `${params.id}` }, data }
+    const expenseUpdated = await prisma.expenses.update(updateExpenseCondiction)
+    try {
+      return expenseUpdated
+    } catch (err) {
+      throw err.message
+    }
+  }
+  async destroy({ params }: HttpContext) {
+    const expenseCondiction = { where: { id: `${params.id}` } }
+    const exepenseToDelete = await prisma.expenses.findUnique(expenseCondiction)
+    try {
+      await prisma.expenses.delete(expenseCondiction)
+      return `Well done! "${exepenseToDelete?.description}", deleted successfully`
+    } catch (err) {
+      throw err.message
+    }
+  }
 }
